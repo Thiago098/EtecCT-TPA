fde0121b24367eeb411a274aec79f6e7ea912a54
Thiago Henrique <69225683+Thiago098@users.noreply.github.com>
2020-11-26 16:36:10 -0300

    Upload do exercício de TPA
    Neste exercício apreendemos a utilizar a função RETURN

---
+++ Função.java
@@ -0,0 +1,23 @@
+class operacoes {
+    
+public static int multiplicacao(int num1, int num2) {
+        
+
+return (num1*num2);
+    }
+   
+
+ public static int divisao(int num1, int num2) {
+      
+
+  return (num1/num2);
+    }
+    
+
+ public static void main (String[]args) {
+        
+ System.out.println(operacoes.multiplicacao(20, 40));
+       
+ System.out.println(operacoes.divisao(10, 20));
+    }
+}
